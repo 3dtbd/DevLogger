1dd5d8a77834ddb96f570f85ed1fd038eb1bf445
Ross Gardler <ross@gardler.org>
2021-06-05 12:04:41 -0700

    Release v0.3.0

---
+++ DevLogger/Editor/VersionConstants.cs
@@ -0,0 +1,33 @@
+namespace org.WizardsCode.DevLogger.Editor
+{
+	/// <summary>
+	/// Version info for this library.
+	/// </summary>
+	internal static class VersionConstants
+	{
+		/// <summary>
+		/// The semantic version
+		/// </summary>
+		public const string VERSION = "0.3.0";
+
+		/// <summary>
+		/// The branch of GIT this package was published from.
+		/// </summary>
+		public const string GIT_BRANCH = "master";
+
+		/// <summary>
+		/// The current GIT commit hash this package was published on.
+		/// </summary>
+		public const string GIT_COMMIT = "e4943c65fb516477b817c6288b60bd70327d7cc9";
+
+		/// <summary>
+		/// The UTC human-readable date this package was published at.
+		/// </summary>
+		public const string PUBLISH_DATE = "Saturday, June 5, 2021";
+
+		/// <summary>
+		/// The UTC time this package was published at.
+		/// </summary>
+		public const string PUBLISH_TIME = "06/05/2021 18:58:53";
+	}
+}
